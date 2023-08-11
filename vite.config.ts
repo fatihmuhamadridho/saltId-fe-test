8c23ec7162485f727506003c90afcd9f460b54a4
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2023-08-11 17:51:55 +0700

    feat: 🎸 config tsconfig and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path-browserify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   server: {
-    port: 3000
+    port: 3000,
   },
   preview: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@assets': path.resolve(new URL('.', import.meta.url).pathname, './src/assets'),
+      '@components': path.resolve(new URL('.', import.meta.url).pathname, './src/components'),
+      '@pages': path.resolve(new URL('.', import.meta.url).pathname, './src/pages'),
+      '@routes': path.resolve(new URL('.', import.meta.url).pathname, './src/routes'),
+      '@styles': path.resolve(new URL('.', import.meta.url).pathname, './src/styles'),
+      '@': path.resolve(new URL('.', import.meta.url).pathname, './src'),
+    },
+  },
+});
